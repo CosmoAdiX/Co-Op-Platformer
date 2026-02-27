f394b9c3a0923e8e52db9fb177ecd43ad05eceed
RyzAdi <adityakumar03332@gmail.com>
2026-02-27 17:50:46 +0530

    adding and attaching the script

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -4,4 +4,10 @@ extends CanvasLayer
 @onready var button_quit: Button = %ButtonQuit
 
 func _ready() -> void:
-	pass
+	button_join.pressed.connect(on_join)
+	button_quit.pressed.connect(on_quit)
+	
+func on_join() -> void:
+	print("JOINED LIL BRO!")
+func on_quit() -> void:
+	print("GTFO BRO!!!")
