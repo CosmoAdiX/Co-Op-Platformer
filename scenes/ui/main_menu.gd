b24f99383224510c64a0e1aeddcadbb1c81c5f06
RyzAdi <adityakumar03332@gmail.com>
2026-03-01 00:48:39 +0530

    attaching the join button to join the forest scene

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -3,9 +3,16 @@ extends CanvasLayer
 @onready var button_join: Button = %ButtonJoin
 @onready var button_quit: Button = %ButtonQuit
 
+const WORLD_FOREST = preload("uid://p7owgavxnbl0")
+
+
+
+
 func _ready() -> void:
 	button_join.pressed.connect(on_join)
 	button_quit.pressed.connect(func(): get_tree().quit())
 	
 func on_join() -> void:
-	print("JOINED LIL BRO!")
+	var new_world = WORLD_FOREST.instantiate()
+	get_tree().current_scene.add_child(new_world)
+	hide()
