55bf6149d9a69d7508e14085f17ad0b31350555a
RyzAdi <adityakumar03332@gmail.com>
2026-04-10 16:59:38 +0530

    sljdljdljsd

--- scripts/global.gd
+++ scripts/global.gd
@@ -13,7 +13,25 @@ func _ready() :
 	Network.tube_client.session_created.connect(set_up_scoreboard)
 
 func set_up_scoreboard():
-	session_info[1]
+	session_info[1] = { "score": 0, "username": username }
+	multiplayer.peer_connected.connect(add_session)
+	multiplayer.peer_disconnected.connect(erased_session)
+	
+func add_session():
+	pass
+	
+func erased_session():
+	pass
+	
+func get_player(peer_id: int) -> Player:
+	var player_to_find: Player
+	for current_player in get_tree().get_nodes_in_group("Players"):
+		if current_player.name == str(peer_id):
+			player_to_find = current_player
+			break
+			
+	# TODO: not found???
+	return player_to_find
 
 @rpc("any_peer", "call_local")
 func shoot_ball(pos, dir, force):
